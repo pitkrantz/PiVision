bf256d0372a0742f567a85feb7f923592c12622f
pitkrantz <pitkrantz@gmail.com>
2022-07-23 20:56:40 +0200

    Camera Test

--- Test1.py
+++ Test1.py
@@ -1,2 +1,14 @@
-for i in range(10):
-    print("Moin Meister")
+import cv2 as cv
+
+capture = cv.VideoCapture(1)
+
+while True:
+    isTrue, frame = capture.read()
+
+    cv.imshow("Video", frame)
+    
+    if cv.waitKey(20) & 0xFF==ord("Q"):
+        break
+
+capture.release()
+cv.destroyAllWindows()
